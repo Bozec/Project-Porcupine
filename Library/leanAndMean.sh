223ef6d40b42a66a7c27e2fa216b825108e31240
Martin "quill18" Glaude <quill18@towerdive.com>
2012-01-02 17:43:30 -0500

    Some potential conflicts, trying a strict gitignore and purging some files.

---
+++ Library/leanAndMean.sh
@@ -0,0 +1,23 @@
+rm assetDatabase3
+rm AssetImportState
+rm assetservercachev3
+rm AssetServerCacheV3
+rm AssetVersioning.db
+rm AudioManager.asset
+rm BuildPlayer.prefs
+rm BuildSettings.asset
+rm DynamicsManager.asset
+rm EditorSettings.asset
+rm EditorUserBuildSettings.asset
+rm expandedItems
+rm FailedAssetImports.txt
+rm guidmapper
+rm InspectorExpandedItems.asset
+rm MonoManager.asset
+rm NetworkManager.asset
+rm ScriptMapper
+rm -r -d -f cache
+rm -r -d -f previews
+rm -r -d -f ScriptAssemblies
+
+
